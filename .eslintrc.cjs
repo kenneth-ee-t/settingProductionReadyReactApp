050f1c55d3c8145695188b992480002e63d07aae
Kenneth Ewonde <Kennethewonde@takeo.ai>
2023-05-01 09:29:06 -0500

    Settup test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,22 +4,28 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    // tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": 0,
+    'react/react-in-jsx-scope': 0,
   },
+  overrides: [
+    {
+      files: ['*.json', '*.json5'], // Specify the extension or pattern you want to parse as JSON.
+      parser: 'jsonc-eslint-parser', // Set this parser.
+    },
+  ],
 };
